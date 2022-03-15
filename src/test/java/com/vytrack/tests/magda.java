a6fd41d03e87637875b1828207be4546fdd99f04
magdalenagru91 <magdalenagru91@gmail.com>
2022-03-14 21:04:48 -0400

    magda second commit

--- src/test/java/com/vytrack/tests/magda.java
+++ src/test/java/com/vytrack/tests/magda.java
@@ -1,6 +1,19 @@
 package com.vytrack.tests;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class magda {
 
-    //HELLLOOOOO
+    @Test(priority = 1)
+    public void test1(){
+        System.out.println("Test 1 is running...");
+
+        //ASSERT EQUALS compare 2 of the same things:
+
+        String actual = "apple";
+        String expected = "apple";
+
+        Assert.assertEquals(actual, expected);
+    }
 }
